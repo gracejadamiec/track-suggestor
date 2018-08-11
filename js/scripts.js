f3c2129446143981a0a412f3c3260ae916ad4444
Grace Adamiec <gracjeadamiec@Graces-MacBook-Air.local>
2018-08-10 18:42:34 -0700

    Displays images instead of alerts for the difrent tracks.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
+
   $("button").click(function(event) {
     event.preventDefault();
   $("form").hide("slow");
-  $("output").show;
 
   var vArt = 0
   var vJava = 0
@@ -18,45 +18,48 @@ $(document).ready(function(){
   var nothing = $("input:radio[name=four]:checked").val();
   console.log(nothing);
 
-  if (design === "designYes") {
-    vArt += 2
-  }
+    if (design === "designYes") {
+      vArt += 2
+    }
 
-  if (java === "javaYes") {
-    vJava += 3
-  }
+    if (java === "javaYes") {
+      vJava += 3
+    }
 
-  if (php ==="phpYes") {
-    vPhp =+ 1
-  }
+    if (php ==="phpYes") {
+      vPhp =+ 1
+    }
 
-  if (nothing === "programingLame"){
-    vNothing += 100
-  }
+    if (nothing === "programingLame"){
+      vNothing += 100
+    }
 
-  if (vNothing > vArt && vNothing > vJava && vNothing > vPhp) {
-    alert("Programing isn't lame! Try Retaking the Survey");
-  }
+    if (vNothing > vArt && vNothing > vJava && vNothing > vPhp) {
+      alert("Programing isn't lame! Try Retaking the Survey");
+    }
 
 
-  if (vArt > vJava && vArt > vNothing && vArt > vPhp) {
-    alert("Design");
-  }
+    if (vArt > vJava && vArt > vNothing && vArt > vPhp) {
+      $("#cssImg").show();
 
-  if (vPhp > vNothing && vPhp > vArt && vPhp > vJava) {
-    alert("Php");
-  }
+    }
 
-  if (vJava > vPhp && vJava > vArt && vJava > vNothing) {
-    alert("Java");
-  }
+    if (vPhp > vNothing && vPhp > vArt && vPhp > vJava) {
+      alert("You Should try Learning PHP!");
+      // $("#p").show(); I could NOT get this to work!
+    }
 
-  console.log(vArt);
-  console.log(vJava);
-  console.log(vPhp);
-  console.log(vNothing);
+    if (vJava > vPhp && vJava > vArt && vJava > vNothing) {
+      // alert("Java");
+      $("#javaImg").show();
+    }
 
-  $('input[type="radio"]').prop('checked', false);
+    console.log(vArt);
+    console.log(vJava);
+    console.log(vPhp);
+    console.log(vNothing);
 
-  });
+    $('input[type="radio"]').prop('checked', false);
+
+    });
 });
